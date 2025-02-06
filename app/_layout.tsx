a99f3ba9b67d9c6578c2942ccfc6cdab31886955
nebalus <coding@nebalus.dev>
2025-02-06 03:07:37 +0100

    Goofing around with some stuff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,33 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
+import {StatusBar} from "expo-status-bar";
+import {useColorScheme} from "@/hooks/useColorScheme";
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,54 +1,17 @@
-import { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
-import * as Location from 'expo-location';
 export default function Index() {
-    const [location, setLocation] = useState<Location.LocationObject | null>(null);
-    const [errorMsg, setErrorMsg] = useState<string | null>(null);
-
-    useEffect(() => {
-        async function getCurrentLocation() {
-
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({
-                accuracy: Location.Accuracy.Highest,
-                enableHighAccuracy: true,
-                timeout: 3000,
-            });
-            setLocation(location);
-        }
-
-        getCurrentLocation();
-    }, []);
-
-    let text = 'Waiting...';
-    if (errorMsg) {
-        text = errorMsg;
-    } else if (location) {
-        text = JSON.stringify(location);
-    }
-
     return (
-        <View style={styles.container}>
-            <Text style={styles.paragraph}>{text}</Text>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Link href="/sign-in">SIGN IN</Link>
+            <Link href="/tests/GetLocation">GetLocation</Link>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20,
-    },
-    paragraph: {
-        fontSize: 18,
-        textAlign: 'center',
-    },
-});
